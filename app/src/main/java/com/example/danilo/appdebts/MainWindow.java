9c1e87465b182349c3712185be871d2a55ecf869
gabo8090 <g-araujo94@hotmail.com>
2019-07-04 12:00:17 -0300

    Gabriel Oliveira

--- app/src/main/java/com/example/danilo/appdebts/MainWindow.java
+++ app/src/main/java/com/example/danilo/appdebts/MainWindow.java
@@ -7,6 +7,7 @@ import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -45,6 +46,9 @@ public class MainWindow extends AppCompatActivity {
         mLayout = findViewById(R.id.layout);
         createConnection();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+
+
         mListDebts.setLayoutManager(linearLayoutManager);
         mDebtsAdapter = new DebtsAdapter(mDebtsDAO.listDebts());
         mListDebts.setAdapter(mDebtsAdapter);
