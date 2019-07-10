75e3407c16e4f5e62b82cf0264160f5cdd663aeb
gabo8090 <g-araujo94@hotmail.com>
2019-07-10 11:53:03 -0300

    Gabriel Oliveira

---
+++ app/src/main/java/com/example/danilo/appdebts/InsertDebts.java
@@ -0,0 +1,47 @@
+package com.example.danilo.appdebts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class InsertDebts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_debts);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled( true); //Ativar o botão
+        getSupportActionBar().setTitle(R.string.titleInsert);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add_debts,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id. home: //ID do seu botão (gerado automaticamente pelo android, usando comoestá, deve funcionar
+                startActivity( new Intent(this, MainWindow. class)); //O efeito ao ser pressionado do botão(no caso abre a activity)
+                finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+}
